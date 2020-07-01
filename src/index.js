fa08b8ed66679841822f77b84050204a82696983
sidhant <sidhantsharma293@gmail.com>
2020-07-01 15:17:04 +0530

    index.css removed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-// import './index.css';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { RoomProvider } from './context';
